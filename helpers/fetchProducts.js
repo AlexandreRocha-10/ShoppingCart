4144bdb00e2fdc797f70711d12c74fbf9a302611
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-03-11 17:44:14 -0300

    feat: cria cors

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const fetchProducts = async (computador) => {
   if (!computador || computador !== 'computador') { throw new Error('You must provide an url'); }
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`;
